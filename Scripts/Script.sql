ff2ae69a0cf2002214c25237cc44be9d56c3e0d4
osram21 <osram21@naver.com>
2017-07-10 21:10:56 +0900

    exerd 스크립트 완성

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -14,10 +14,16 @@ CREATE TABLE board (
 	board_no        int      NOT null AUTO_increment, -- 게시물 번호
 	member_no       int      NULL,     -- 회원 번호
 	board_title     VARCHAR(100) NOT NULL, -- 게시물 제목
-	board_content   TEXT         NULL,     -- 게시글 내용
 	board_count     int      NOT NULL, -- 조회수
 	board_startdate datetime     NOT null,  -- 작성일자
 	PRIMARY KEY (board_no)
+);
+
+-- 게시글 내용
+create table content(
+	board_no int not null AUTO_increment,
+	board_content text null,
+	primary key(board_no)
 );
 
 -- 댓글
@@ -30,9 +36,6 @@ CREATE TABLE reply (
 	PRIMARY KEY (reply_no)
 );
 
-alter table reply add constraint fk_board
-foreign key(board_no) references board(board_no);
-
 -- 업로드
 CREATE TABLE upload (
 	upload_no   int          not null AUTO_increment,
@@ -42,8 +45,6 @@ CREATE TABLE upload (
 	primary key(upload_no)
 );
 
-drop table upload;
-
 -- 포인트장소
 CREATE TABLE point (
 	point_no      int      NOT null AUTO_increment, -- 포인트 번호
@@ -51,15 +52,19 @@ CREATE TABLE point (
 	point_file    VARCHAR(200) NULL,     -- 사진 파일
 	point_where   VARCHAR(200) NOT NULL, -- 장소 
 	point_title   VARCHAR(100) NOT NULL, -- 글 제목
-	point_latiude  int         NULL, -- 위도
-	point_hardness int         null, -- 경도
-	point_content TEXT         NULL,     -- 글 내용
+	point_latiude  double         NULL, -- 위도
+	point_hardness double         null, -- 경도
 	point_count   int      NOT NULL, -- 조회수
 	point_date    datetime     null,      -- 글 날짜
 	PRIMARY KEY (point_no)
 );
 
-drop table point;
+create table pt_content(
+	point_no int not null AUTO_increment,
+	point_content text null,
+	primary key(point_no)
+);
+
 -- 포인트 댓글
 CREATE TABLE point_reply (
 	pr_no      int   NOT null AUTO_increment, -- 포인트댓글번호
@@ -72,29 +77,20 @@ CREATE TABLE point_reply (
 	primary key(pr_no)
 );
 
-alter table point_reply add constraint fk_point
-foreign key(point_no) references point(point_no);
-
-drop table point_reply;
 -- 랭크
 CREATE TABLE score (
 	member_no int NOT NULL, -- 회원 번호
 	point_no  int NOT null,  -- 포인트 번호
 	primary key(member_no,point_no)
 );
-alter table score add constraint fk_member
-foreign key(member_no) references member(member_no);
-
-ALTER TABLE score add CONSTRAINT FK_point_TO_score 
-FOREIGN KEY (point_no) REFERENCES point (point_no);
 
 create table event(
 	event_no int not null AUTO_increment, -- 행사번호
 	event_date datetime not null, -- 행사 기간
 	event_place varchar(100) not null, -- 행사 장소
-	event_latiude varchar(100) null, -- 행사 위도
-	event_hardness varchar(100) null, -- 행사 경도
-	event_homepage varbinary(100) not null, -- 홈페이지
+	event_latiude double null, -- 행사 위도
+	event_hardness double null, -- 행사 경도
+	event_homepage varchar(100) not null, -- 홈페이지
 	event_intro varchar(1000) not null, -- 행사 소개
 	event_content varchar(1000) not null, -- 행사 내용
 	event_title varchar(100) not null, -- 행사 타이틀
@@ -107,3 +103,30 @@ create table event_img(
 	img_file varchar(200) not null, -- 행사 이미지 파일
 	primary key(img_no)
 );
+
+alter table reply add constraint fk_board
+foreign key(board_no) references board(board_no);
+
+alter table point_reply add constraint fk_point
+foreign key(point_no) references point(point_no);
+
+alter table score add constraint fk_member
+foreign key(member_no) references member(member_no);
+
+ALTER TABLE score add CONSTRAINT FK_point_TO_score 
+FOREIGN KEY (point_no) REFERENCES point (point_no);
+
+ALTER TABLE content add CONSTRAINT FK_board_TO_content 
+FOREIGN KEY (board_no) REFERENCES board (board_no);
+
+
+ALTER TABLE pt_content add CONSTRAINT FK_point_TO_pt_content
+FOREIGN KEY (point_no) REFERENCES point (point_no);
+
+drop table member;
+drop table relply;
+drop table board;
+drop table upload;
+drop table point_reply;
+drop table point;
+drop table score;
