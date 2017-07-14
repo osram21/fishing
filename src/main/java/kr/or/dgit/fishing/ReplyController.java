6e7454e0f1be262c62a517515605324b7e5be31b
osram21 <osram21@naver.com>
2017-07-14 17:33:55 +0900

    부트 넣고 댓글 ing...존나어려움

--- src/main/java/kr/or/dgit/fishing/ReplyController.java
+++ src/main/java/kr/or/dgit/fishing/ReplyController.java
@@ -1,6 +1,8 @@
 package kr.or.dgit.fishing;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.or.dgit.domain.Criteria;
+import kr.or.dgit.domain.PageMaker;
 import kr.or.dgit.domain.Reply;
 import kr.or.dgit.service.ReplyService;
 
@@ -50,5 +54,56 @@ public class ReplyController {
 		return entity;
 	}
 	
+	@RequestMapping(value="/{replyNo}",method=RequestMethod.DELETE)
+	public ResponseEntity<String>delete(@PathVariable("replyNo")int replyNo){
+		ResponseEntity<String> entity = null;
+		try {
+			service.replyDel(replyNo);
+			entity = new ResponseEntity<String>(HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	@RequestMapping(value="/{replyNo}",method=RequestMethod.PUT)
+	public ResponseEntity<String>update(@PathVariable("rno")int replyNo, @RequestBody Reply r){
+		ResponseEntity<String> entity = null;
+		try {
+			r.setReplyNo(replyNo);
+			service.replyUpdate(r);
+			entity = new ResponseEntity<String>(HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
 	
+	@RequestMapping(value="{boardNo}/{page}",method=RequestMethod.GET)
+	public ResponseEntity<Map<String, Object>>listPage(@PathVariable("boardNo")int boardNo,@PathVariable("page")int page){
+		ResponseEntity<Map<String, Object>> entity = null;
+		try {
+			//페이지 넣고
+			Criteria cri = new Criteria();
+			cri.setPage(page);
+			List<Reply> list = service.listPage(boardNo, cri);
+			
+			//페이지에 대한 카운터 시키는거 70개면 페이징 7개 생성 이런거?? 맞나?? 그러겟지
+			PageMaker pageMaker = new PageMaker();
+			pageMaker.setCri(cri);
+			pageMaker.setTotalCount(service.count(boardNo));
+			
+			//위에 두개를 맵으로 받아서 보내버림
+			Map<String, Object> map = new HashMap<>();
+			map.put("list", list);
+			map.put("pageMaker", pageMaker);
+			
+			entity = new ResponseEntity<>(map,HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
 }
--- src/main/webapp/WEB-INF/views/board/read.jsp
+++ src/main/webapp/WEB-INF/views/board/read.jsp
@@ -5,10 +5,10 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<!-- <link rel="stylesheet"   href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet"   href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <link rel="stylesheet"   href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
- <script src='resources/calender/js/jquery.min.js'></script>
-<script   src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> -->
+<!--  <script src='resources/calender/js/jquery.min.js'></script> -->
+<script   src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
@@ -36,12 +36,51 @@ $(function () {
 		$("#f1").submit();
 	});
 	$("#list").click(function(){
-		location.href="listAll";
+		location.href="listPage";
 		//$("#f1").attr("action","listPage");
 		//$("#f1").attr("method","get");
 		//$("#f1").submit();
 	});
 });
+</script>
+	<!-- <div>
+		<h1>댓글을 넣어봅시다</h1>
+			<label>작성자</label>
+			<input type="text" placeholder="아이디" id="replywriter"><br>
+			<label>댓글 내용</label>
+			<input type="text" placeholder="내용 적기" id="replycontent">
+	</div>
+	<button>댓글 추가</button>
+		<div>
+			<ul>
+				<li>댓글 리스트</li>
+			</ul>
+		</div> -->
+		<div class="row"><!-- 댓글 -->
+		<div class="col-md-12">
+			<div class="box box-success">
+				<div class="box-header">
+					<h3 class="box-title">댓글 추가</h3>
+				</div>
+				<div class="box-body">
+					<label for="newReplyWriter">작성자</label>
+					<input type="text" placeholder="아이디" id="newReplyWriter" class="form-control">
+					<br>
+					<label for="newReplyText">댓글내용</label>
+					<input type="text" placeholder="Reply Text" id="newReplyText" class="form-control">
+				</div>
+				<div class="box-footer">
+					<button class="btn btn-primary" id="btnadd">댓글 추가</button>
+				</div>
+			</div><!-- 댓글 추가 -->
+			<%-- <ul class="timeline">
+				<li class="time-label" id="repliesList">
+					<span class="bg-green" id="btnList">댓글 리스트<small id="replycnt">[${board.reply }]</small></span>
+				</li>
+			</ul>  --%>
+		</div>
+<script type="text/javascript">
+	var page_bar = 1;
 </script>
 </body>
 </html>
